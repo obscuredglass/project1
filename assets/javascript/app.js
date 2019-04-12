e9fa1dc612e61c86833026cec3d255dccfc11d37
michael Lorzing <ghostsofgrace@gmail.com>
2019-04-12 11:21:39 -0400

    commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+<<<<<<< HEAD
     var rapidAPI = "7ba93ae7bfmsh489625be869c93bp1fc810jsne3c26e878543";
 
     $("#search-button").on("click", function (event) {
@@ -69,4 +70,33 @@ $(document).ready(function () {
 
     //    displayMovieFunction()
     //leave me here!
+=======
+  var rapidAPI = "7ba93ae7bfmsh489625be869c93bp1fc810jsne3c26e878543";
+
+  function displayMovieFunction() {
+    $("#search-button").on("click", function (event) {
+      event.preventDefault()
+      var movie = $("#user-input").attr("data-name");
+
+      var queryURL = "http://www.omdbapi.com/?t=" + movie + "&apikey=d62e414d";
+
+      $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+        .then(function (response) {
+          console.log(queryURL);
+          console.log(response);
+
+          var results = response.data;
+          var $rtTr = $("<tr>")
+          $rtTr.text(results);
+          $("#search-results").append($rtTr);
+        })
+    });
+  };
+
+
+  //leave me here!
+>>>>>>> commit
 });
